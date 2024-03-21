7fa69e65cae3882245c389b54880356788276727
Patrick Lavin <prlavin@sandia.gov>
2024-03-21 07:15:01 -0700

    add autotools to sst installer

--- scripts/init-sst.sh
+++ scripts/init-sst.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/env bash
 set -euo pipefail
+set -x
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 CORE_BRANCH=master
 ELEMENTS_BRANCH=devel
+
+M4_VER=1.4.19
+AC_VER=2.72
+AM_VER=1.16.5
+LT_VER=2.4.7
 
 if [ -z ${SST_INSTALL+x} ];
 then
@@ -11,12 +17,41 @@ then
     exit
 fi
 
+mkdir -p $SCRIPT_DIR/autotools
+cd autotools
+wget https://ftp.gnu.org/gnu/m4/m4-$M4_VER.tar.xz
+wget https://ftp.gnu.org/gnu/autoconf/autoconf-$AC_VER.tar.xz
+wget https://ftp.gnu.org/gnu/automake/automake-$AM_VER.tar.xz
+wget https://ftp.gnu.org/gnu/libtool/libtool-$LT_VER.tar.xz
+
+tar xf m4-$M4_VER.tar.xz
+tar xf autoconf-$AC_VER.tar.xz
+tar xf automake-$AM_VER.tar.xz
+tar xf libtool-$LT_VER.tar.xz
+
+cd m4-$M4_VER
+./configure --prefix=$SST_INSTALL
+make all install
+
+cd ../autoconf-$AC_VER
+./configure --prefix=$SST_INSTALL
+make all install
+
+cd ../automake-$AM_VER
+./configure --prefix=$SST_INSTALL
+make all install
+
+cd ../libtool-$LT_VER
+./configure --prefix=$SST_INSTALL
+make all install
+
+cd $SCRIPT_DIR
+
 if [ -n ${INTEL_PIN_DIRECTORY+x} ];
 then
     if [ ! -d ${INTEL_PIN_DIRECTORY} ];
     then
         echo "Error: INTEL_PIN_DIRECTORY variable set but does not point to a directory. Please install pin at $INTEL_PIN_DIRECTORY"
-        echo "  -> https://www.intel.com/content/www/us/en/developer/articles/tool/pin-a-binary-instrumentation-tool-downloads.html"
         exit
     fi
 fi
