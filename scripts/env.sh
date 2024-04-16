92055ce74dcd77855ad9731ce970809c8174afd1
Patrick Lavin <plavin997@gmail.com>
2024-04-16 12:43:55 -0400

    update env.sh

--- scripts/env.sh
+++ scripts/env.sh
@@ -7,11 +7,3 @@ export INTEL_PIN_DIR=${INTEL_PIN_DIRECTORY}
 export PIN_HOME=${INTEL_PIN_DIRECTORY}
 export PATH=${SST_INSTALL}/bin/:$PATH
 
-module load mpi/openmpi-4.1.4
-
-# Activate Spack and environment
-#source ~/spack/share/spack/setup-env.sh
-#spack load ecp-proxy-apps@1.0
-#spack load ecp-proxy-apps@4.0
-#spack env activate ATS-5
-#spack env activate ecp1.0
