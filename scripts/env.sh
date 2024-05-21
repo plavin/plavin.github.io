a268541f89f97b0fc37860671995526a253c76d9
Patrick Lavin <prlavin@sandia02.srn.sandia.gov>
2024-05-21 14:34:15 -0600

    add new env var

--- scripts/env.sh
+++ scripts/env.sh
@@ -2,6 +2,7 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 export SST_INSTALL=${SCRIPT_DIR}/install
+export SST_ELEMENTS_ROOT=${SCRIPT_DIR}/sst-elements
 export INTEL_PIN_DIRECTORY=${SCRIPT_DIR}/pin-3.28
 export INTEL_PIN_DIR=${INTEL_PIN_DIRECTORY}
 export PIN_HOME=${INTEL_PIN_DIRECTORY}
